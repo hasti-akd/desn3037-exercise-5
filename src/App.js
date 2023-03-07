c1b04d8b29a164944d330f5a0a9b6314262ef55b
hasti-akd <hasti.akbarideilami@georgebrown.ca>
2023-03-07 16:18:33 -0500

    New custom components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import * as React from "react";
+
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import About from "./components/About";
+import Clients from "./components/Clients";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-     
+
+      <BrowserRouter>
+
+        <Routes>
+          <Route exact path="about" element={<About />} />
+          <Route exact path="clients" element={<Clients />} />
+          <Route exact path="contact" element={<Contact />} />
+        </Routes>
+
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/Clients.js
@@ -0,0 +1,10 @@
+export default function Clients(){
+
+    return(
+
+        <div>
+            Clients Page
+        </div>
+
+    )
+}
