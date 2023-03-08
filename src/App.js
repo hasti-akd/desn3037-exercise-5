bd43f393eea95b5fe9a2c0bf5d6eacbd62af74f5
hasti-akd <hasti.akbarideilami@georgebrown.ca>
2023-03-07 22:15:55 -0500

    Menu system creation with current page status

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import About from "./components/About";
 import Clients from "./components/Clients";
 import Contact from "./components/Contact";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
 
       <BrowserRouter>
+
+        <Menu />
 
         <Routes>
           <Route exact path="about" element={<About />} />
---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import { Tab, Tabs } from "@mui/material";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+
+export default function Menu(){
+
+    const {pathname} = useLocation();
+
+    return(
+
+        <div>
+            <Tabs value={pathname}>
+                <Tab component = {Link} to="/about" value="/about" label="About" />
+                <Tab component = {Link} to="/clients" value="/clients" label="Clients" />
+                <Tab component = {Link} to="/contact" value="/contact" label="Contact" />
+            </Tabs>
+        </div>
+
+    )
+}
